3ffb4f7fe885e1212b7d3e265e82fbe5b9b43176
Nexus Agente IA Hibrido de Última Geração <lucas.thomaz.ia@gmail.com>
2026-08-23 21:50:49 -0300

    fusion: src/firebase/config.ts (clone do secundario)

---
+++ src/firebase/config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  "projectId": "studio-861496498-500a3",
+  "appId": "1:227333393760:web:42c780e605821a66052205",
+  "apiKey": "AIzaSyCpWX0Lc2cn9SpZaLWyTZL02PE1-xY0FHY",
+  "authDomain": "studio-861496498-500a3.firebaseapp.com",
+  "messagingSenderId": "227333393760"
+};
